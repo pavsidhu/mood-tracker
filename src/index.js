b49529d48a0d0d77290f155f9a91bdb7960527c7
Pav Sidhu <pav@pavsidhu.com>
2018-06-04 02:44:35 +0100

    Replace CSS with styled-components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { injectGlobal } from 'styled-components'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+      Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-size: 62.5%;
+}
+`
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
